0283462ba24fd72e0072c5225acad1d071a59df4
Dushyant kaushik <39647887+drajhari4418@users.noreply.github.com>
2026-09-19 17:04:53 +0530

    Switch Firebase data layer to Realtime Database

--- js/firebase-app.js
+++ js/firebase-app.js
@@ -1,26 +1,27 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
-import { getFirestore, doc, getDoc, setDoc, collection, addDoc, getDocs, query, orderBy, limit, onSnapshot, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { getDatabase, ref, get, set, push, update, onValue } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
 import { firebaseConfig } from "./firebase-config.js";
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getDatabase(app);
 export const googleProvider = new GoogleAuthProvider();
-export { GoogleAuthProvider, signInWithPopup, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, onAuthStateChanged, doc, getDoc, setDoc, collection, addDoc, getDocs, query, orderBy, limit, onSnapshot, serverTimestamp };
+
+export { GoogleAuthProvider, signInWithPopup, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, onAuthStateChanged, ref, get, set, push, update, onValue };
 
 export async function ensureUserProfile(user) {
-  const ref = doc(db, "users", user.uid);
-  const snap = await getDoc(ref);
+  const userRef = ref(db, `users/${user.uid}`);
+  const snap = await get(userRef);
   if (!snap.exists()) {
-    await setDoc(ref, {
+    await set(userRef, {
       uid: user.uid,
       name: user.displayName || "",
       email: user.email || "",
       photoURL: user.photoURL || "",
       role: "user",
-      createdAt: serverTimestamp()
-    }, { merge: true });
+      createdAt: Date.now()
+    });
   }
-  return (await getDoc(ref)).data();
+  return (await get(userRef)).val();
 }
