389c685a58dbe788bdc50c90ae0644a26d90ce75
Dushyant kaushik <39647887+drajhari4418@users.noreply.github.com>
2026-09-22 00:52:26 +0530

    Migrate legacy Firebase services into React source

---
+++ src/services/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBvf_5b5YhAB6_eg0vapjLNA0dSR9Ap5lo",
+  authDomain: "tirth-yatra-3a5f2.firebaseapp.com",
+  projectId: "tirth-yatra-3a5f2",
+  storageBucket: "tirth-yatra-3a5f2.firebasestorage.app",
+  messagingSenderId: "783898250589",
+  appId: "1:783898250589:web:b9d26e4e4e4bce4da66cb4",
+  measurementId: "G-6LF6RHHPRT",
+  databaseURL: "https://tirth-yatra-3a5f2-default-rtdb.europe-west1.firebasedatabase.app"
+};
